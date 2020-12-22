e556a65ae3339ef8a1ab9aa43ce36ac64f52d8b8
Zach Olstein <zach@transposit.com>
2020-12-22 00:50:33 -0800

    flatten grid

--- sudoku.py
+++ sudoku.py
@@ -13,39 +13,34 @@ grid = [
  [1, 0, 7, 0, 0, 0, 2, 5, 0],
 ]
 
-# grid = [cell for row in grid for cell in row]
+grid = [cell for row in grid for cell in row]
     
-def is_possible(grid, x, y, v):
-    row_pairs = ((i, y) for i in range(9))
-    col_pairs = ((x, j) for j in range(9))
-    x_corner = x // 3 * 3
-    y_corner = y // 3 * 3
-    box_pairs = ((x_corner + i_off, y_corner + j_off) 
-                 for i_off in range(3) 
-                 for j_off in range(3))
-    pairs = it.chain(row_pairs, col_pairs, box_pairs)
-    return not any(grid[i][j] == v for (i, j) in pairs)
+def is_possible(grid, index, v):
+    row_start = index // 9 * 9
+    row_pairs = range(row_start, row_start + 9)
+    col_start = index % 9
+    col_pairs = range(col_start, col_start + 81, 9)
+    corner = index // 27 * 27 + (index % 9) // 3 * 3
+    box_pairs_1 = range(corner, corner + 3)
+    box_pairs_2 = range(corner + 9, corner + 12)
+    box_pairs_3 = range(corner + 18, corner + 21)
+    pairs = it.chain(row_pairs, col_pairs, box_pairs_1, box_pairs_2, box_pairs_3)
+    return not any(grid[n] == v for n in pairs)
 
 def copy(grid):
-    return [row[:] for row in grid]
-
-def get(grid, x, y):
-    return grid[x][y]
-    # return grid[x * 9 + y]
+    return grid[:]
 
 def solve(grid, start=0):
     # solutions = []
     for n in range(start, 9 * 9):
-        x = n // 9
-        y = n % 9
-        if not grid[x][y]:
+        if not grid[n]:
             for v in range(1, 10):
-                if is_possible(grid, x, y, v):
-                    grid[x][y] = v
+                if is_possible(grid, n, v):
+                    grid[n] = v
                     # solutions.extend(solve(grid, n + 1))
                     if solve(grid, n + 1):
                         return grid
-                    grid[x][y] = 0
+                    grid[n] = 0
             # return solutions
             return None
     #return [copy(grid)]
@@ -53,12 +48,11 @@ def solve(grid, start=0):
 
 def print_grid(grid):
 
-    # def chunks(l, n):
-        # for i in range(0, len(l), n):
-            # yield l[i:i + n]
+    def chunks(l, n):
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
 
-    # for row in chunks(grid, 9):
-    for row in grid:
+    for row in chunks(grid, 9):
         print(" ".join((str(x) for x in row)))
 
 print("before")
